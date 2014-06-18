483627ea5c0b81ce9520e9c4ee8e14478c81f7d9
Jesse Shawl <jesse@jshawl.com>
2014-06-18 10:32:16 -0400

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'digest/sha1'
 
+config.serve_static_assets = true
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
