244ff27fd7b0fd0f7fe420b73a07defcf6c37641
Jesse Shawl <jesse@jshawl.com>
2014-06-20 11:17:01 -0400

    refactor

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,24 +1,29 @@
 class Url < ActiveRecord::Base
 
-  before_save :get_css
+  before_save :fetch
+
+  def fetch
+    @link = self.link
+    @hostname = URI( @link ).host
+    @path = 'public/'+ @hostname
+    @now = Time.now.to_i.to_s
+    @doc = Nokogiri::HTML(open( @link ))
+    @css = get_css
+  end
 
   def get_css
-    hostname = URI( self.link ).host
-    Dir.mkdir('public/'+hostname) unless Dir.exist?( 'public/'+hostname )
-    link = self.link
-    now = Time.now.to_i.to_s
-    @doc = Nokogiri::HTML(open( link ))
-    @css_tags = @doc.css('[rel="stylesheet"]').map { |l| URI.join( link, l['href'] ).to_s }
-    # open temp file for writing
-    file = File.open( 'public/'+hostname+'/'+now + '.css','w') { | f |
+    Dir.mkdir( @path ) unless Dir.exist?( @path )
+    @css_tags = @doc.css('[rel="stylesheet"]').map { |l| URI.join( @link, l['href'] ).to_s }
+    file = File.open( @path + @now + '.css','w') { | f |
       @css_tags.each do | c |
 	contents = open(c).read
 	f.write( contents )
       end
     }
-    hash = Digest::MD5.hexdigest(File.read('public/'+hostname+'/'+now+'.css'))
+    hash = Digest::MD5.hexdigest(File.read(@path+@now+'.css'))
     p hash
-    File.rename('public/'+hostname+'/'+now+'.css', 'public/'+hostname+'/'+hash+".css")
-    self.css = hostname+'/'+hash+".css"
+    File.rename(@path+@now+'.css', @path+hash+".css")
+    self.css = @hostname+'/'+hash+".css"
+    self.css
   end
 end
