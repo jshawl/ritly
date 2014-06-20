46c9c88cd3b0b3e69a2a5eaf5d9f483305d3b954
Jesse Shawl <jesse@jshawl.com>
2014-06-20 10:06:59 -0400

    move fetch logic to model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,20 @@
 class Url < ActiveRecord::Base
+  before_save :get_css
+  def get_css
+    link = self.link
+    now = Time.now.to_i.to_s
+    @doc = Nokogiri::HTML(open( link ))
+    @css_tags = @doc.css('[rel="stylesheet"]').map { |l| URI.join( link, l['href'] ).to_s }
+    # open temp file for writing
+    file = File.open( now + '.css','w') { | f |
+      @css_tags.each do | c |
+	contents = open(c).read
+	f.write( contents )
+      end
+    }
+    hash = Digest::MD5.hexdigest(File.read(now+'.css'))
+    p hash
+    File.rename(now+'.css', hash+".css")
+    self.css = hash+".css"
+  end
 end
